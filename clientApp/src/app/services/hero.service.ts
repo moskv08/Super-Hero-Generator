1db20cefea51dbff87522aa91dbb9188879a200f
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-18 17:55:58 +0200

    implement getHeroName function

--- clientApp/src/app/services/hero.service.ts
+++ clientApp/src/app/services/hero.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Superhero } from '../models/superhero';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  superHero: Superhero;
 
   constructor() { }
+
+  getSuperHeroName(user: User) {
+    // http call to backend
+    this.superHero = new Superhero('Black', 'Mamba');
+    return this.superHero;
+  }
 }
