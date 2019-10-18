aeeddebe73bd34e2139769af5d05b17e4a12c0fc
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-18 17:18:32 +0200

    create a super hero class

---
+++ clientApp/src/app/models/superhero.spec.ts
@@ -0,0 +1,7 @@
+import { Superhero } from './superhero';
+
+describe('Superhero', () => {
+  it('should create an instance', () => {
+    expect(new Superhero()).toBeTruthy();
+  });
+});
---
+++ clientApp/src/app/models/superhero.ts
@@ -0,0 +1,7 @@
+export class Superhero {
+    heroName: string;
+
+    constructor(firstname: string, lastname: string) {
+        this.heroName = `${firstname} ${lastname}`;
+    }
+}
