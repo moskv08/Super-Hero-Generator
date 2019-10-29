9a2ba397d39e3b34c7cd5600d6ef8a0b8db8b31d
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-29 22:19:34 +0100

    cleaned up hero service

--- clientApp/src/app/services/hero.service.ts
+++ clientApp/src/app/services/hero.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  superHero: string;
-
   constructor(private http: HttpClient) { }
 
-  getSuperHeroName(user: User) {
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  getSuperHeroName(user: User): Observable<any> {
     // http call to backend
     // return this.http.get('assets/testdata.json');
     return this.http.get('https://7mo9pdmdbd.execute-api.eu-west-1.amazonaws.com/development/superhero');
