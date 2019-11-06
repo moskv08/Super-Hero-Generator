4c65dbf2577ad927f139b0ac1b6fed2f46b5f64b
Fabrizio <Fabrizio_39@hotmail.com>
2019-11-06 22:09:15 +0100

    print response

--- server/deployment/30_invoke.sh
+++ server/deployment/30_invoke.sh
@@ -3,5 +3,7 @@
 
 aws lambda invoke \
 --function-name $functionName \
---payload '{ "color": "A", "power": "A" }' \
+--payload '{ "color": "A", "power": "A2" }' \
     response.json
+# print
+cat response.json
