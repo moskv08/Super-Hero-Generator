5429b675f72af928f8f8ca40cc58cd6e0bfb3984
Fabrizio <Fabrizio_39@hotmail.com>
2019-11-06 18:00:14 +0100

    script to deploy the function

---
+++ server/deployment/20_create-lambda-function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. deployment/my.config
+
+# Delete function
+aws lambda delete-function \
+--function-name $functionName
+
+#  Create function
+aws lambda create-function \
+--function-name $functionName \
+--runtime nodejs10.x \
+--role arn:$ARN \
+--handler $functionName.handler \
+--zip-file fileb://$functionName.zip
