10fb4cf2602191a8717cf249599fe72458dda73e
Fabrizio <Fabrizio_39@hotmail.com>
2019-11-11 20:14:29 +0100

    add additional tests

--- server/tests/generateSuperHero.spec.js
+++ server/tests/generateSuperHero.spec.js
@@ -1,29 +1,40 @@
 const assert = require('assert');
-const { expect } = require('chai');
+// const { expect } = require('chai');
 const should = require('chai').should();
 
 const SuperHero = require('../functions/generateSuperHero');
 
 describe('GenerateSuperHero', function () {
 
-    describe('GetSuperHero', function () {
+    describe('GetHeroName', function () {
 
         it('should not accept several letters per parameter.', function () {
-
             const result = SuperHero.GetHeroName('Hello', 'World');
             result.should.equal('Something went wrong.');
         });
 
         it('should not accept blank parameters', function () {
-
             const result = SuperHero.GetHeroName('', '');
             result.should.equal('Something went wrong.');
         });
 
         it('should not accept numbers.', function () {
-
             const result = SuperHero.GetHeroName('3', '5');
             result.should.equal('Please use single capital letters.');
+        });
+
+        it('should accept a capital letter per parameter.', function () {
+            const result = SuperHero.GetHeroName('A', 'A');
+            result.should.not.equal('Something went wrong.');
+            result.should.not.equal('Please use single capital letters.');
+
+        });
+
+        it('should accept a status code, a header and a body.', function () {
+            const result = SuperHero.GetHeroName('A', 'B');
+            result.should.have.property('statusCode');
+            result.should.have.property('headers');
+            result.should.have.property('body');
         });
     });
 });
