4a1c0d0a1b5eeea7ed670bd7fa0b13e5714ef211
Fabrizio <Fabrizio_39@hotmail.com>
2019-12-23 23:30:24 +0100

    integrate async/await for db calls

--- server/database/ddb_query.js
+++ server/database/ddb_query.js
@@ -6,20 +6,78 @@ AWS.config.update({
 });
 
 // Create the DynamoDB service object
-var ddb = new AWS.DynamoDB.DocumentClient();
+// var ddb = new AWS.DynamoDB.DocumentClient();
 
-var params = {
-    TableName: 'SuperHeroColorCollation',
-    Key: {
-        "letter": "F",
-    },
-};
+// var params = {
+//     TableName: 'SuperHeroColorCollation',
+//     Key: {
+//         "letter": "F",
+//     },
+// };
 
-// Call DynamoDB to read the item from the table
-ddb.get(params, function (err, data) {
-    if (err) {
-        console.log("Error", err);
+// // Call DynamoDB to read the item from the table
+// ddb.get(params, function (err, data) {
+//     if (err) {
+//         console.log("Error", err);
+//     } else {
+//         console.log("Heroname: ", data.Item.supername);
+//     }
+// });
+
+exports.handler = async function (event) {
+    let result = "Something went wrong.";
+    const color = 'M'
+    const power = 'Z'
+
+    let myColor;
+    let myPower;
+
+    if (color.length && power.length == 1) {
+        if (/^[A-Z]/.test(color) && /^[A-Z]/.test(power)) {
+            LoadDataFromDb(color, 'SuperHeroColorCollation').then(result => {
+                console.log(result);
+            }).catch(err => {
+                myColor = 'ERROR: ' + err;
+            });
+
+            LoadDataFromDb(power, 'SuperHeroPowerCollation').then(result => {
+                console.log(result);
+            }).catch(err => {
+                myPower = 'ERROR: ' + err;
+            });
+
+            await 
+
+        }
     } else {
-        console.log("Success", data.Item);
+        result = "Something went wrong. More than one letter detected."
     }
-});
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": JSON.stringify(result),
+        "isBase64Encoded": false
+    };
+}
+
+async function LoadDataFromDb(letter, tableName) {
+    // Create the DynamoDB service object
+    const ddb = new AWS.DynamoDB.DocumentClient();
+
+    const params = {
+        TableName: tableName,
+        Key: {
+            "letter": letter,
+        },
+    };
+
+    try {
+        let res = await ddb.get(params).promise();
+        return res.Item.supername;
+    } catch (err) {
+        console.log(err.message);
+    }
+}
