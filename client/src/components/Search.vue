cb4fdb3327719ccf15eb54844a5c31f9061813d7
Robb Moskv <fabrizio_39@hotmail.com>
2020-02-23 21:03:12 +0100

    implemente name form

---
+++ client/src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="search">
+    <h1>Get your Super Hero Name</h1>
+    <!-- v.on handles events -->
+    <form v-om:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="surname">Surname</label>
+        <input type="text" id="surname" name="surname" v-model="surname" placeholder="John" />
+      </div>
+      <div class="form-group">
+        <label for="givenname">Givenname</label>
+        <input type="text" id="givenname" name="surname" v-model="givenname" placeholder="Doe"/>
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Get Super Hero Name" class="btn">
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      surname: "", 
+      givenname: ""
+    }
+  },
+  beforeCreate(){
+    document.body.className = "body-bg-image";
+  }
+};
+</script>
+
+<style>
+</style>
