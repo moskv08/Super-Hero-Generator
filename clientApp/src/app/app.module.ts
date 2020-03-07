929affda8f5495a2c52b6b49d78a859b84293dbf
Robb Moskv <fabrizio_39@hotmail.com>
2020-03-07 11:32:57 +0100

    declare profile component

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroService } from './services/hero.service';
 import { HeroFormComponent } from './components/hero-form/hero-form.component';
+import { HeroProfileComponent } from './components/hero-profile/hero-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroFormComponent,
+    HeroProfileComponent,
   ],
   imports: [
     BrowserModule,
