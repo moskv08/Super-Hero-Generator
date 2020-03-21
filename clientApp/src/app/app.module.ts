fdb465e5e9d55eb47419b44608efa5ce1e0a93ac
Robb Moskv <fabrizio_39@hotmail.com>
2020-03-21 13:47:10 +0100

    add routing

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroService } from './services/hero.service';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SearchComponent } from './components/search/search.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'profile', component: ProfileComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +24,11 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
+    HttpClientModule, 
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
