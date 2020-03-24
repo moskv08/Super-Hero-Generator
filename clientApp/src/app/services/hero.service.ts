581d188f0b67aa667bb028d68ec7aa60f7b35f41
robbmoskv <fabrizio_39@hotmail.com>
2020-03-24 17:56:12 +0100

    linting

--- clientApp/src/app/services/hero.service.ts
+++ clientApp/src/app/services/hero.service.ts
@@ -15,25 +15,25 @@ export class HeroService {
   private key = environment.apiKey;
 
   constructor(private http: HttpClient) { }
-
+  
   // Http Options
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'x-api-key': this.key,
     })
-  };  
+  };
 
   getSuperHeroName(user: User): Observable<any> {
     // http call to backend
-    try {    
+    try {
       this.color = user.givenname[0].toUpperCase();
       this.power = user.surname[0].toUpperCase();
-  
+
       this.url = this.url + `color=${this.color}&power=${this.power}`;
-  
+
       return this.http.get(`${this.url}`, { headers: this.httpOptions.headers});
-      
+
     } catch (error) {
       return error;
     }
